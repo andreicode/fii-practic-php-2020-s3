e67754d824e738a311de60740b2773668745cb63
Andrei Popa <andrei.popa@expertnetwork.ro>
2020-03-20 20:09:58 +0200

    Add OOP examples

---
+++ db-02-query/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// require __DIR__ . '/../db-01-connection/index.php';
+
+// $statement = $conn->prepare("SELECT * FROM my_table");
+// $statement->execute();
+// $statement->setFetchMode(PDO::FETCH_ASSOC);
+// $results = $statement->fetchAll();
+
+
+// echo '<pre>';
+// foreach($results as $row) {
+//     var_dump($row);
+// }
+// echo '</pre>';
---
+++ db-04-insert/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// require __DIR__ . '/../db-01-connection/index.php';
+
+// $conn->exec("INSERT INTO my_table (id, nume) VALUE (NULL, 'Testare')");
